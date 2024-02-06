cdde906a9c857e277f8b201e004dde409d4bdfa2
Nimit Jain <nimitjain258@gmail.com>
2024-02-06 23:57:47 +0200

    adding additional files

---
+++ insert_additional_tables.sql
@@ -0,0 +1,27 @@
+INSERT INTO branch_supplier VALUES
+	(2, 'Hammer Mill', 'Paper'),
+    (2, 'Uni-ball', 'Writing Utensils'),
+    (3, 'Patriot Paper', 'Paper'),
+    (2, 'J.T. Forms & Labels', 'Custom Forms'),
+    (3, 'Uni-ball', 'Writing Utensils'),
+    (3, 'Hammer Mill', 'Paper'),
+    (3, 'Stamford Lables', 'Custom Forms');
+    
+INSERT INTO client VALUES
+	(400, 'Dunmore Highschool', 2),
+    (401, 'Lackawana Country', 2),
+    (402, 'FedEx', 3),
+    (403, 'John Daly Law, LLC', 3),
+    (404, 'Scranton Whitepages', 2),
+    (405, 'Times Newspaper', 3),
+    (406, 'FedEx', 2);
+    
+INSERT INTO works_with VALUES
+	(105, 400, 55000),
+    (102, 401, 267000),
+    (108, 402, 22500),
+    (107, 403, 5000),
+    (108, 403, 12000),
+    (105, 404, 33000),
+	(102, 406, 15000),
+    (105, 406, 130000);
